6ed12d1b7340a8575a8b5ea9baf526d25133c36d
StrangeGreyCat <gxtrus@gmail.com>
2020-05-10 23:10:47 +0300

    Stage #2: Equalizing chances
    Description
    
    Well, now let's do something real. Nobody wants the game where you always lose.
    Using the power of the random module, we'll make a truly interesting game.
    
    You should write a program that reads input from the user, chooses a random option and then says who won, the user or the computer.
    There are a few examples below, providing output for any outcome (<option> is the option chosen by your program):
    
    Lose -> Sorry, but computer chose <option>
    Draw -> There is a draw (<option>)
    Win -> Well done. Computer chose <option> and failed
    Objectives
    
    Your program should:
    
    Read user's input specifying the option that user has chosen
    Choose a random option
    Compare the options and determine the winner
    Output a line depending on the result of the game:
    Lose -> Sorry, but computer chose <option>
    Draw -> There is a draw (<option>)
    Win -> Well done. Computer chose <option> and failed

--- Rock-Paper-Scissors/task/rps/game.py
+++ Rock-Paper-Scissors/task/rps/game.py
@@ -1,8 +1,15 @@
+import random
+
+win_table = {'scissors': {'scissors': 'draw', 'rock': 'lose', 'paper': 'win'},
+        'rock'         : {'scissors': 'win', 'rock': 'draw', 'paper': 'lose'},
+        'paper'        : {'scissors': 'lose', 'rock': 'lose', 'paper': 'draw'}}
+item_list = ['scissors', 'rock', 'paper']
 a = input()
-if a == 'scissors':
-    b = 'rock'
-elif a == 'rock':
-    b = 'paper'
-else:
-    b = 'scissors'
-print(f'Sorry, but computer chose {b}')
+b = random.choice(item_list)
+result = win_table[a][b]
+if result == 'draw':
+    print(f'There is a draw ({b})')
+elif result == 'win':
+    print(f'Well done. Computer chose {b} and failed')
+elif result == 'lose':
+    print(f'Sorry, but computer chose {b}')
