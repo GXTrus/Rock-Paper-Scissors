4603fcf4ea411ba242bc3a217b79d61b8827901f
StrangeGreyCat <gxtrus@gmail.com>
2020-05-11 07:22:26 +0300

    Stage #3: Endless game
    Description
    
    Wasn't that pretty cool?
    
    But the game is really short so far: nobody plays just a single shot of rock-paper-scissors. We need to do some literally unstoppable game for unstoppable players. Not literally unstoppable, of course: let's implement a way to stop your program.
    
    Improve your program so it would take an unlimited number of inputs until the user enters !exit. After entering !exit the program should print Bye! and terminate. Also, let's try to handle invalid inputs: your program should be ready that there may be a typo in user's input, or that a user may just enter complete gibberish instead of a normal command. So, in case the input doesn't correspond to any known command (option name or !exit), your program should output the line Invalid input
    
    Objectives
    
    Your program should:
    
    Take an input from the user
    If the input is !exit, output Bye! and stop the game
    If the input is the name of the option, then:
    Pick a random option
    Output a line with the result of the game in the following format (<option> is the name of the option chosen by the program):
    Lose -> Sorry, but computer chose <option>
    Draw -> There is a draw (<option>)
    Win -> Well done. Computer chose <option> and failed
    If the input corresponds to anything else, output Invalid input
    Do it all over again

--- Rock-Paper-Scissors/task/rps/game.py
+++ Rock-Paper-Scissors/task/rps/game.py
@@ -1,15 +1,27 @@
 import random
 
-win_table = {'scissors': {'scissors': 'draw', 'rock': 'lose', 'paper': 'win'},
-        'rock'         : {'scissors': 'win', 'rock': 'draw', 'paper': 'lose'},
-        'paper'        : {'scissors': 'lose', 'rock': 'lose', 'paper': 'draw'}}
-item_list = ['scissors', 'rock', 'paper']
-a = input()
-b = random.choice(item_list)
-result = win_table[a][b]
-if result == 'draw':
-    print(f'There is a draw ({b})')
-elif result == 'win':
-    print(f'Well done. Computer chose {b} and failed')
-elif result == 'lose':
-    print(f'Sorry, but computer chose {b}')
+
+class RockPaperScissors:
+
+    def __init__(self):
+        self.win_combinations = {'scissors': 'paper', 'paper': 'rock', 'rock': 'scissors'}
+        self.item_list = ['scissors', 'rock', 'paper']
+        self.game()
+
+    def game(self):
+        check = True
+        while check:
+            self.human = input()
+            if self.human in self.item_list:
+                self.cpu = random.choice(self.item_list)
+                if self.human == self.cpu: print(f'There is a draw ({self.cpu})')
+                elif self.win_combinations[self.human] == self.cpu:
+                    print(f'Well done. Computer chose {self.cpu} and failed')
+                else: print(f'Sorry, but computer chose {self.cpu}')
+            elif self.human == '!exit':
+                check = False
+                print('Bye!')
+            else: print("Invalid input")
+
+
+RockPaperScissors()
